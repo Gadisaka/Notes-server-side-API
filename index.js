8eff2c43aa45fbd252fd31289e0aeded4319ebb3
gadisa kasaye <gadisaka04@gmail.com>
2024-11-09 23:25:15 +0300

    bind server to all network interfaces for accessibility

--- index.js
+++ index.js
@@ -67,6 +67,6 @@ app.use((err, req, res, next) => {
 
 // Start the server
 // const PORT = process.env.PORT || 3000;
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
   console.log(`Server is running on port ${port}`);
 });
